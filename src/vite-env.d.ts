52e08ca3f7c96797172e78f847c643db5fc08de5
AFCMS <afcm.contact@gmail.com>
2025-02-04 12:01:59 +0100

    Document Supabase env variables

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,7 +1,21 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
+  /**
+   * The URL of the Supabase API.
+   *
+   * Typically, this is `https://<your-project-id>.supabase.co` or `http://127.0.0.1:54321` for local development.
+   *
+   * @see https://supabase.com/docs/guides/getting-started/quickstarts/reactjs
+   */
   readonly VITE_SUPABASE_URL: string;
+  /**
+   * The anonymous key of the Supabase API.
+   *
+   * This key is used to authenticate with the Supabase API.
+   *
+   * @see https://supabase.com/docs/guides/getting-started/quickstarts/reactjs
+   */
   readonly VITE_SUPABASE_ANON_KEY: string;
 }
 

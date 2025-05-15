436509cf3a91ae9959ede0132ccf0f9870062b1c
Hue <emilien.dessarps@gmail.com>
2025-05-15 12:44:27 +0200

    fixed linter problems

--- src/Components/PostViewer/PostViewer.tsx
+++ src/Components/PostViewer/PostViewer.tsx
@@ -2,8 +2,8 @@ import { Tables } from "../../contexts/supabase/database";
 
 export default function PostViewer(props: { post: Tables<"posts"> }) {
   // On formate la date pour l'afficher dans le format français car j'aime trop la france la chienneté
-  const dateCreation = new Date(props.post.created_at).toLocaleDateString('fr-FR');
-  
+  const dateCreation = new Date(props.post.created_at).toLocaleDateString("fr-FR");
+
   return (
     <div>
       <p>ID: {props.post.id}</p>
@@ -11,9 +11,9 @@ export default function PostViewer(props: { post: Tables<"posts"> }) {
       <p>Date de création: {dateCreation}</p>
       {props.post.media && (
         <div>
-          {props.post.media.match(/\.(jpeg|jpg|gif|png)$/i) ? (
+          {/\.(jpeg|jpg|gif|png)$/i.exec(props.post.media) ? (
             <img src={props.post.media} alt="Image du post" width="300" />
-          ) : props.post.media.match(/\.(mp4|webm|ogg)$/i) ? (
+          ) : /\.(mp4|webm|ogg)$/i.exec(props.post.media) ? (
             <video src={props.post.media} controls width="300" />
           ) : (
             <a href={props.post.media}>Voir la pièce jointe</a>

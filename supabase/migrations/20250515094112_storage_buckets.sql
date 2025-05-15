8d6cdd9251a4d094886ed451a92641df771610d0
Hue <emilien.dessarps@gmail.com>
2025-05-15 11:58:33 +0200

    fixed storage buckets not being created

---
+++ supabase/migrations/20250515094112_storage_buckets.sql
@@ -0,0 +1,11 @@
+insert into storage.buckets
+(id, name, public)
+VALUES ('post-media', 'post-media', true);
+
+insert into storage.buckets
+(id, name, public)
+VALUES ('banners', 'banners', true);
+
+insert into storage.buckets
+(id, name, public)
+VALUES ('profile-pics', 'profile-pics', true);

36f6a09d050412eb21d0b02ebc43c9c99341a5ea
AFCMS <afcm.contact@gmail.com>
2025-05-27 12:01:43 +0200

    PWA first pass

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,45 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "@tailwindcss/vite";
+import { VitePWA, VitePWAOptions } from "vite-plugin-pwa";
+
+const pwaOptions: Partial<VitePWAOptions> = {
+  strategies: "generateSW",
+  registerType: "autoUpdate",
+  mode: "production",
+  base: "/",
+  includeAssets: ["favicon.svg"],
+  minify: true,
+  manifest: {
+    name: "Malley",
+    short_name: "Malley",
+    theme_color: "#000000",
+    icons: [
+      {
+        src: "pwa-64x64.png",
+        sizes: "64x64",
+        type: "image/png",
+      },
+      {
+        src: "pwa-192x192.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
+      {
+        src: "pwa-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+      },
+      {
+        src: "maskable-icon-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+        purpose: "maskable",
+      },
+    ],
+  },
+};
 
 export default defineConfig({
-  plugins: [tailwindcss(), react()],
+  plugins: [tailwindcss(), react(), VitePWA(pwaOptions)],
 });

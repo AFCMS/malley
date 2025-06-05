bfd3cc84408172adb22c534b8e0136e4e655ad8e
AFCMS <afcm.contact@gmail.com>
2025-06-05 15:35:03 +0200

    Working top navbar
    Also unifies the way popovers are handled

--- src/layouts/TopNavbar/TopNavbar.tsx
+++ src/layouts/TopNavbar/TopNavbar.tsx
@@ -2,6 +2,7 @@ import { Link } from "react-router";
 
 import { useAuth } from "../../contexts/auth/AuthContext";
 import { utils } from "../../contexts/supabase/supabase";
+import { closePopover } from "../../utils/popover";
 
 export default function TopNavbar() {
   const auth = useAuth();
@@ -10,21 +11,37 @@ export default function TopNavbar() {
     <nav className="navbar bg-base-100 border-b border-slate-200 shadow-sm lg:hidden">
       <div className="navbar-start">
         {auth.isAuthenticated && auth.profile && (
-          <div className="dropdown">
-            <div tabIndex={0} role="button" className="avatar">
+          <div className="">
+            <button
+              className="avatar"
+              popoverTarget="popover-top-navbar-profile"
+              style={{ anchorName: "--popover-top-navbar-profile" } as React.CSSProperties}
+            >
               <div className="w-10 rounded-full">
                 <img src={utils.getAvatarUrl(auth.profile)} />
               </div>
-            </div>
-            <ul tabIndex={0} className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+            </button>
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown dropdown-bottom dropdown-start bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
+              popover="auto"
+              id="popover-top-navbar-profile"
+              style={{ positionAnchor: "--popover-top-navbar-profile" } as React.CSSProperties}
+            >
               <li>
-                <a>Homepage</a>
+                <button
+                  className=""
+                  onClick={() => {
+                    void auth.logout();
+                  }}
+                >
+                  Logout
+                </button>
               </li>
               <li>
-                <a>Portfolio</a>
-              </li>
-              <li>
-                <a>About</a>
+                <Link to="/settings" onClick={closePopover("popover-top-navbar-profile")}>
+                  Settings
+                </Link>
               </li>
             </ul>
           </div>

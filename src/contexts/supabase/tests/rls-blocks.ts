8e592155869d517248b452c1fb094f6c7f49bc3f
Hue <emilien.dessarps@gmail.com>
2025-06-07 11:00:40 +0200

    added security policies for profile-pics and banners buckets added tests for those policies

--- src/contexts/supabase/tests/rls-blocks.ts
+++ src/contexts/supabase/tests/rls-blocks.ts
@@ -1,5 +1,4 @@
 import { describe, expect, test } from "vitest";
-
 import { queries, supabase } from "../supabase";
 import { randomName, registerAndLoginNewUser, createRandomPost } from "./supabase.test-utils";
 
@@ -123,6 +122,159 @@ export function rls_blocks() {
         const req2 = await supabase.from("features").delete().eq("featuree", user2.id).eq("featurer", user1.id);
         expect(req2.error == null).toBe(true);
       });
+    });
+
+    describe("profile pictures", () => {
+      test("cannot upload file with invalid name", async () => {
+        await registerAndLoginNewUser();
+        const dummyFile = new File(["avatar-data"], "avatar.png", { type: "image/png" });
+        // This filename does NOT start with the user's UUID
+        const fileName = "invalidprefix_" + Math.floor(Date.now() / 1000).toString() + ".png";
+
+        const req = await supabase.storage.from("profile-pics").upload(fileName, dummyFile, {
+          cacheControl: "3600",
+          upsert: false,
+        });
+
+        expect(req.error).not.toBeNull();
+      });
+
+      test("cannot upload a second profile pic for the same user", async () => {
+        const { user } = await registerAndLoginNewUser();
+        const dummyFile1 = new File(["avatar-data"], "avatar1.png", { type: "image/png" });
+        await expect(queries.profiles.updateAvatar(dummyFile1)).resolves.not.toThrow();
+
+        const dummyFile2 = new File(["avatar-data"], "avatar2.png", { type: "image/png" });
+        const extension = dummyFile2.type.split("/")[1] || "png";
+        const fileName = user.id + "_" + Math.floor(Date.now() / 1000).toString() + "." + extension;
+        const req = await supabase.storage.from("profile-pics").upload(fileName, dummyFile2, {
+          cacheControl: "3600",
+          upsert: false,
+        });
+
+        expect(req.error).not.toBeNull();
+      });
+
+      test("cannot upload profile pic when not logged in", async () => {
+        // also serves as impostor test
+        const { user } = await registerAndLoginNewUser();
+        await supabase.auth.signOut();
+
+        // Use the valid naming convention
+        const extension = "png";
+        const fileName = user.id + "_" + Math.floor(Date.now() / 1000).toString() + "." + extension;
+        const dummyFile = new File(["avatar-data"], "avatar.png", { type: "image/png" });
+
+        const req = await supabase.storage.from("profile-pics").upload(fileName, dummyFile, {
+          cacheControl: "3600",
+          upsert: false,
+        });
+
+        expect(req.error).not.toBeNull();
+      });
+
+      test("cannot remove another user's profile pic", async () => {
+        let { user: userA } = await registerAndLoginNewUser();
+        const dummyFile = new File(["avatar-data"], "avatar.png", { type: "image/png" });
+        await queries.profiles.updateAvatar(dummyFile);
+
+        await supabase.auth.signOut();
+        await registerAndLoginNewUser();
+        userA = await queries.profiles.get(userA.id);
+        if (userA.profile_pic == null) {
+          throw new Error();
+        }
+        await supabase.storage.from("profile-pics").remove([userA.profile_pic]);
+
+        const req = await supabase.storage.from("profile-pics").download(userA.profile_pic);
+        expect(req.error).toBeNull();
+        expect(req.data).not.toBeNull();
+      });
+    });
+
+    describe("storage", () => {
+      const registerPersonalFilesNegativeTests = (
+        updateFn: (file: File | null) => Promise<void>,
+        bucketName: "profile-pics" | "banners",
+        testName: "profile picture" | "banner",
+        field: "profile_pic" | "banner",
+      ) => {
+        describe(`storage (${bucketName}) - negative ${testName}`, () => {
+          test(`cannot upload ${testName} with invalid name`, async () => {
+            await registerAndLoginNewUser();
+            const dummyFile = new File(["dummy"], "file.png", { type: "image/png" });
+            // Filename does NOT start with the user's UUID
+            const fileName = "invalidprefix_" + Math.floor(Date.now() / 1000).toString() + ".png";
+
+            const req = await supabase.storage.from(bucketName).upload(fileName, dummyFile, {
+              cacheControl: "3600",
+              upsert: false,
+            });
+
+            expect(req.error).not.toBeNull();
+          });
+
+          test(`cannot upload a second ${testName} for the same user`, async () => {
+            const { user } = await registerAndLoginNewUser();
+            const dummyFile1 = new File(["dummy"], "file1.png", { type: "image/png" });
+            await expect(updateFn(dummyFile1)).resolves.not.toThrow();
+
+            const dummyFile2 = new File(["dummy"], "file2.png", { type: "image/png" });
+            const extension = dummyFile2.type.split("/")[1] || "png";
+            const fileName = user.id + "_" + Math.floor(Date.now() / 1000).toString() + "." + extension;
+            const req = await supabase.storage.from(bucketName).upload(fileName, dummyFile2, {
+              cacheControl: "3600",
+              upsert: false,
+            });
+
+            expect(req.error).not.toBeNull();
+          });
+
+          test(`cannot upload ${testName} when not logged in`, async () => {
+            const { user } = await registerAndLoginNewUser();
+            await supabase.auth.signOut();
+
+            const extension = "png";
+            const fileName = user.id + "_" + Math.floor(Date.now() / 1000).toString() + "." + extension;
+            const dummyFile = new File(["dummy"], "file.png", { type: "image/png" });
+
+            const req = await supabase.storage.from(bucketName).upload(fileName, dummyFile, {
+              cacheControl: "3600",
+              upsert: false,
+            });
+
+            expect(req.error).not.toBeNull();
+          });
+
+          test(`cannot remove another user's ${testName}`, async () => {
+            let { user: userA } = await registerAndLoginNewUser();
+            const dummyFile = new File(["dummy"], "file.png", { type: "image/png" });
+            await updateFn(dummyFile);
+
+            await supabase.auth.signOut();
+            await registerAndLoginNewUser();
+            userA = await queries.profiles.get(userA.id);
+            if (!userA[field]) {
+              throw new Error();
+            }
+            const filePath = userA[field];
+            await supabase.storage.from(bucketName).remove([filePath]);
+
+            const req = await supabase.storage.from(bucketName).download(filePath);
+            expect(req.error).toBeNull();
+            expect(req.data).not.toBeNull();
+          });
+        });
+      };
+
+      registerPersonalFilesNegativeTests(
+        queries.profiles.updateAvatar,
+        "profile-pics",
+        "profile picture",
+        "profile_pic",
+      );
+
+      registerPersonalFilesNegativeTests(queries.profiles.updateBanner, "banners", "banner", "banner");
     });
   });
 }

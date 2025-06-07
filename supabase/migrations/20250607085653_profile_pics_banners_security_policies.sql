9b25ae256c8119e9ff134762286a595f6d7d71f6
Hue <emilien.dessarps@gmail.com>
2025-06-07 11:00:40 +0200

    added security policies for profile-pics and banners buckets added tests for those policies

---
+++ supabase/migrations/20250607085653_profile_pics_banners_security_policies.sql
@@ -0,0 +1,84 @@
+drop policy "user_upload 1tghu4n_0" on "storage"."objects";
+
+drop policy "user_upload 8o6wzb_0" on "storage"."objects";
+
+drop policy "user_upload jh40r1_0" on "storage"."objects";
+
+drop policy "any_read 1tghu4n_0" on "storage"."objects";
+
+drop policy "any_read jh40r1_0" on "storage"."objects";
+
+CREATE UNIQUE INDEX one_banner_per_user ON storage.objects USING btree (bucket_id, "left"(name, 36));
+
+CREATE UNIQUE INDEX one_profile_pic_per_user ON storage.objects USING btree (bucket_id, "left"(name, 36));
+
+create policy "insert own banner 1tghu4n_0"
+on "storage"."objects"
+as permissive
+for insert
+to public
+with check (((bucket_id = 'banners'::text) AND ("left"(name, 36) = (auth.uid())::text) AND ( SELECT (count(*) = 0)
+   FROM storage.objects o
+  WHERE ((o.bucket_id = 'banners'::text) AND ("left"(o.name, length((auth.uid())::text)) = (auth.uid())::text)))));
+
+
+create policy "insert own pfp jh40r1_0"
+on "storage"."objects"
+as permissive
+for insert
+to public
+with check (((bucket_id = 'profile-pics'::text) AND ("left"(name, 36) = (auth.uid())::text) AND ( SELECT (count(*) = 0)
+   FROM storage.objects o
+  WHERE ((o.bucket_id = 'profile_pics'::text) AND ("left"(o.name, length((auth.uid())::text)) = (auth.uid())::text)))));
+
+
+create policy "remove own banner 1tghu4n_0"
+on "storage"."objects"
+as permissive
+for delete
+to public
+using (((bucket_id = 'banners'::text) AND ("left"(name, length((auth.uid())::text)) = (auth.uid())::text)));
+
+
+create policy "remove own pfp jh40r1_0"
+on "storage"."objects"
+as permissive
+for delete
+to public
+using (((bucket_id = 'profile-pics'::text) AND ("left"(name, length((auth.uid())::text)) = (auth.uid())::text)));
+
+
+create policy "update own banner 1tghu4n_0"
+on "storage"."objects"
+as permissive
+for update
+to public
+using (((bucket_id = 'banners'::text) AND ("left"(name, length((auth.uid())::text)) = (auth.uid())::text)));
+
+
+create policy "update pfp jh40r1_0"
+on "storage"."objects"
+as permissive
+for update
+to public
+using (((bucket_id = 'profile-pics'::text) AND ("left"(name, length((auth.uid())::text)) = (auth.uid())::text)));
+
+
+create policy "any_read 1tghu4n_0"
+on "storage"."objects"
+as permissive
+for select
+to public
+using ((bucket_id = 'banners'::text));
+
+
+create policy "any_read jh40r1_0"
+on "storage"."objects"
+as permissive
+for select
+to public
+using ((bucket_id = 'profile-pics'::text));
+
+
+
+

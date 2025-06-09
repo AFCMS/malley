bdb1555e2c84ade6cc10ab337983491521d92352
AFCMS <afcm.contact@gmail.com>
2025-06-09 13:59:33 +0200

    Some functions refactoring

--- supabase/functions/createPost/index.ts
+++ supabase/functions/createPost/index.ts
@@ -1,6 +1,6 @@
-import "jsr:@supabase/functions-js/edge-runtime.d.ts";
-import { createClient } from "jsr:@supabase/supabase-js@2";
-import { corsHeaders } from "../../../_shared/cors.ts";
+import "@supabase/functions-js/edge-runtime.d.ts";
+import { createClient } from "@supabase/supabase-js";
+import { corsHeaders } from "../_shared/cors.ts";
 
 Deno.serve(async (req) => {
   if (req.method === "OPTIONS") {

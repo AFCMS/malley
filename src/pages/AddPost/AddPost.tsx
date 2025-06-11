71a8034bf4f1223a0eae5bb9e7fb07e0cf33fbb7
AFCMS <afcm.contact@gmail.com>
2025-06-11 16:16:32 +0200

    Better AddPost

--- src/pages/AddPost/AddPost.tsx
+++ src/pages/AddPost/AddPost.tsx
@@ -1,41 +1,25 @@
 import { Link, useParams } from "react-router";
-import { useAuth } from "../../contexts/auth/AuthContext";
 import PostAdd from "../../Components/PostAdd/PostAdd";
+import TopBar from "../../layouts/TopBar/TopBar";
 
 export default function AddPost() {
-  const auth = useAuth();
   const { postId } = useParams<{ postId: string }>();
 
   // Détermine si on est en mode édition
   const isEditMode = Boolean(postId);
 
   return (
-    <div className="flex w-full flex-col px-4">
-      <h1 className="mb-4 text-xl font-bold">{isEditMode ? "Modifier le Post" : "Ajouter un Post"}</h1>
+    <div className="flex w-full flex-col">
+      <TopBar title={isEditMode ? "Edit Post" : "Add Post"} />
+      <div className="mt-4 px-4">
+        <PostAdd editPostId={postId} showCategories={true} showFileUpload={true} />
 
-      <PostAdd editPostId={postId} showCategories={true} showFileUpload={true} />
-
-      <div className="mt-4">
-        <Link className="btn btn-primary" to={isEditMode ? `/post/${postId ?? ""}` : "/"}>
-          {isEditMode ? "Retour au post" : "Retour à l'accueil"}
-        </Link>
-      </div>
-
-      <div className="mt-8">
-        {auth.isAuthenticated && auth.profile && auth.user ? (
-          <>
-            <div className="mb-2">{auth.profile.handle}</div>
-          </>
-        ) : (
-          <>
-            <div className="mb-2">Non connecté</div>
-            <Link className="mr-2 rounded-md bg-amber-100 p-2" to="/login">
-              Connexion
+        {isEditMode && (
+          <div className="mt-4">
+            <Link className="btn btn-primary" to={`/post/${postId ?? ""}`}>
+              Back to Post
             </Link>
-            <Link className="rounded-md bg-amber-100 p-2" to="/register">
-              Inscription
-            </Link>
-          </>
+          </div>
         )}
       </div>
     </div>
